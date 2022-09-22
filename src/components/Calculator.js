d11cc7d94a6a4c51e11e388f67e35427a6db14a1
Fraol Kelbessa <fraolkelbessa@gmail.com>
2022-09-22 12:46:18 +0300

    first commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ResultDisplay from './ResultDisplay'
+
+export default class Calculator extends Component {
+  render() {
+	return (
+	  <div>
+		<ResultDisplay/>
+		Calculator
+		
+	  </div>
+	)
+  }
+}
---
+++ src/components/ResultDisplay.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ResultDisplay extends Component {
+  render() {
+	return (
+	  <div>ResultDisplay</div>
+	)
+  }
+}
